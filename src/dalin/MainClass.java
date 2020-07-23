9d30fc50f43e5f1f085e5daf2501df00bd5086e0
KimChangGue <rlackdrb2904@naver.com>
2020-07-23 09:45:12 +0900

    연산의 달인 수정본

--- src/dalin/MainClass.java
+++ src/dalin/MainClass.java
@@ -8,7 +8,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		SubClass start = new SubClass();
 		for (;;) {
-			start.SubClass(10);
+			System.out.print("문제 수 정하기 > ");
+			try {
+			Scanner scan = new Scanner(System.in);
+			int a = scan.nextInt();
+			start.SubClass(a);
+			}catch (Exception e) {
+				System.out.println("잘못 입력하셨습니다. 숫자를 입력해주세요");
+			}
 		}
 	}
 }
